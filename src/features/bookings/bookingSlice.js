7f740de4d0bdd8504935a78f865c736820d7ec9e
Izzat <izzatmuz@gmail.com>
2024-10-14 13:51:02 +0800

    removed bookingSlice

--- src/features/bookings/bookingSlice.js
+++
@@ -1,6 +0,0 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import axios from "axios"
-import { jwtDecode } from "jwt-decode"
-
-const BASE_URL = 'https://00eedf02-2e35-4427-971f-90cc71844922-00-2m2f638mfzuk2.sisko.replit.dev'
-
